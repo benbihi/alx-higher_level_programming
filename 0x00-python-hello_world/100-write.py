6928830d82cde048db9b013c2ae5b89320e945d0
benbihi <elm.benbihi@gmail.com>
2024-03-12 17:57:22 -0700

    Task 100 agaaain

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-ERROR_MESSAGE = "This is a custom error message: 'and that piece of art is useful - Dora Korpar, 2015-10-19'"
-
-def main():
-  """Main function for error handling and message output."""
-  sys.stderr.write(f"{ERROR_MESSAGE}\n")
-  sys.exit(1)
-
-if __name__ == "__main__":
-  main()
-
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
