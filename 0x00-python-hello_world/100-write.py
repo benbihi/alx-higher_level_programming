f76546c16cc5a6af41cc35bc5ce3877c902135ee
benbihi <elm.benbihi@gmail.com>
2024-03-12 17:53:03 -0700

    Task 100 again

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
 
-sys.stderr.write(message)
-exit(1)
 
