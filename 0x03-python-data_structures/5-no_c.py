7f7033fe1b470467b9070b3f67c71575981dc633
benbihi <elm.benbihi@gmail.com>
2024-03-18 19:15:53 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+# 5-no_c.py
+
+
 def no_c(my_string):
-    return(my_string.translate({ord('C'): None, ord('c'): None}))
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
