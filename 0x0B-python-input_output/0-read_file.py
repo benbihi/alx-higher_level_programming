c939b42fe561c1f3e7d9a957e74c398107835de1
benbihi <elm.benbihi@gmail.com>
2024-04-16 17:37:29 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """read file utf8 con with """
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
+            print(line, end='')
