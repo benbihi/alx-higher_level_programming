d2f20b4d8c700241967f9e26af097dcab5e11f3e
benbihi <elm.benbihi@gmail.com>
2024-06-28 15:40:23 -0700

    task 11 semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,18 +2,18 @@
 const args = process.argv.slice(2).map(Number);
 
 if (args.length < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    let max = -Infinity;
-    let secondMax = -Infinity;
+  let max = -Infinity;
+  let secondMax = -Infinity;
 
-    for (const num of args) {
-        if (num > max) {
-            secondMax = max;
-            max = num;
-        } else if (num > secondMax && num < max) {
-            secondMax = num;
-        }
+  for (const num of args) {
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax && num < max) {
+      secondMax = num;
     }
-    console.log(secondMax);
+  }
+  console.log(secondMax);
 }
