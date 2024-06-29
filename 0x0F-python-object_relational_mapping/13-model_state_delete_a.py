7abd276afcb60b0abbd27a176eef6ee504ab8232
benbihi <elm.benbihi@gmail.com>
2024-06-28 17:03:15 -0700

    task 13 2

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states:
-        session.delete(state)
+    # states = session.query(State).filter(State.name.like('%a%')).all()
+    # for state in states:
+        # session.delete(state)
+    for st in session.query(State).filter(State.name.like('%a%')):
+        session.delete(st)
     session.commit()
     session.close()
